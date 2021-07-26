87266fcad016469104b4f19dc50257f834d37226
keimoriyama <keischwiiz@gmail.com>
2021-07-26 22:43:08 +0900

    react-visで遊ぶ

--- frontend/src/chart.tsx
+++ frontend/src/chart.tsx
@@ -1,3 +1,30 @@
-import React from 'react';
-import "react-vis/dist/style.css";
-import { XYPlot, LineSeries } from "react-vis";
+import React, {Component} from 'react';
+import "../node_modules/react-vis/dist/style.css"
+import {XYPlot, LineSeries,VerticalBarSeries,  VerticalGridLines, HorizontalGridLines, XAxis, YAxis} from "react-vis";
+
+
+class Chart extends Component {
+    render(){
+        const data = [
+      {x: 0, y: 8},
+      {x: 1, y: 5},
+      {x: 2, y: 4},
+      {x: 3, y: 9},
+      {x: 4, y: 1},
+      {x: 5, y: 7},
+    ];
+        return (
+            <XYPlot xType="ordinal" height={200} width={200}>
+            <VerticalGridLines />
+            <HorizontalGridLines />
+            <XAxis />
+                <YAxis />
+                <VerticalBarSeries barWidth={0.5} data={data} />
+                <LineSeries data={data} />
+        </XYPlot>
+    )
+    }
+}
+
+
+export default Chart
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Chart from './chart'
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <Chart />
   </React.StrictMode>,
   document.getElementById('root')
 );
