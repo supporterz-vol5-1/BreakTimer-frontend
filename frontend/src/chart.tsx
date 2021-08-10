bcc4c357fed8f64640f919dec9f661ddea3435b0
keimoriyama <keischwiiz@gmail.com>
2021-08-10 09:59:31 +0900

    データからキーのみを抽出

--- frontend/src/chart.tsx
+++ frontend/src/chart.tsx
@@ -1,17 +1,23 @@
 import React, {PureComponent} from 'react';
 import {BarChart, Bar, XAxis, YAxis, Tooltip, Legend, ResponsiveContainer} from 'recharts'
-
-const data = [
-  {x: 0, y: 8},
-  {x: 1, y: 5},
-  {x: 2, y: 4},
-  {x: 3, y: 9},
-  {x: 4, y: 1},
-  {x: 5, y: 7},
-];
+import data from './data.json'
 
 class Chart extends PureComponent {
+    handleData(data: any) {
+        var lang_list: string[] = [];
+        // console.log(data)
+        for (var item of data) {
+            console.log(item)
+            Object.keys(item).forEach(key => {
+                if(!lang_list.includes(key)){
+                    lang_list.push(key)
+                }
+            })
+        }
+        console.log(lang_list)
+    }
     render(){
+        this.handleData(data)
         return (
             <BarChart width={500} height={300} data={data} margin={{ top: 20, right: 30, left: 0, bottom: 0 }}>
                 <XAxis dataKey="x" />
