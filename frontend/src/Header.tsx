4009206411e667e0101269faea9261ac757243e5
keimoriyama <keischwiiz@gmail.com>
2021-08-13 10:45:12 +0900

    インデントの調整

--- frontend/src/Header.tsx
+++ frontend/src/Header.tsx
@@ -1,11 +1,11 @@
 import React, {Component} from 'react'
 
 class Header extends Component{
-render(){
-    return(
-        <div>header</div>
-    )
-}
+    render(){
+        return(
+            <div>header</div>
+        )
+    }
 }
 
 export default Header
