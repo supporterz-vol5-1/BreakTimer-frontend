6308b76952e24bca7721bad728d3548ad1306de9
keimoriyama <keischwiiz@gmail.com>
2021-08-13 10:49:17 +0900

    combined app.tsx

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Chart from './Chart'
+import App from './App'
 
 ReactDOM.render(
     <React.StrictMode>
-        <Chart />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
