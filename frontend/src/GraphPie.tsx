71ec9917f36409ce51aa391d5788259c9edfed50
keimoriyama <keischwiiz@gmail.com>
2021-08-15 09:40:30 +0900

    add dataformant script

--- frontend/src/GraphPie.tsx
+++ frontend/src/GraphPie.tsx
@@ -1,6 +1,26 @@
 import React from "react"
 
-const GraphPie = () =>{
+import { PieChart, Pie, Legend, Tooltip  } from "recharts"
+
+type Props = {
+    time_data: Array<Object>
+}
+
+const GraphPie = (props: Props) => {
+    const chartdata:Array<Object> = props.time_data
+    var dataDict: {[name: string]: number} = {}
+    for(var elem of chartdata){
+        // console.log(elem)
+        Object.entries(elem).forEach(([key,value]) => {
+            if(!(key in dataDict)){
+                dataDict[key] = value
+            }
+            else{
+                dataDict[key] += value
+            }
+        })
+    }
+    console.log(dataDict)
     return(
         <div> aaaa </div>
     ) 
