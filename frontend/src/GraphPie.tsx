1a5ff4ef3fd78eae5268944b7ac81e41d125a3f0
keimoriyama <keischwiiz@gmail.com>
2021-08-15 10:11:07 +0900

    円グラフを表示できた

--- frontend/src/GraphPie.tsx
+++ frontend/src/GraphPie.tsx
@@ -6,10 +6,15 @@ type Props = {
     time_data: Array<Object>
 }
 
-const GraphPie = (props: Props) => {
-    const chartdata:Array<Object> = props.time_data
+type ChartData = {
+    lang: string
+    time: number
+}
+
+const exportData = (data: Array<Object>) => {
     var dataDict: {[name: string]: number} = {}
-    for(var elem of chartdata){
+    // 一回集計
+    for(var elem of data){
         // console.log(elem)
         Object.entries(elem).forEach(([key,value]) => {
             if(!(key in dataDict)){
@@ -20,9 +25,28 @@ const GraphPie = (props: Props) => {
             }
         })
     }
-    console.log(dataDict)
-    return(
-        <div> aaaa </div>
+    // console.log(dataDict)
+    return dataDict
+}
+
+const GraphPie = (props: Props) => {
+    const chartdata: Array<Object> = props.time_data
+    var data = exportData(chartdata)
+    var tmp: ChartData;
+    var formattedData: Array<ChartData> = []
+    Object.entries(data).forEach(([key, value]) => {
+        console.log(key, value)
+        tmp = {
+            lang : key,
+            time: value
+        }
+        formattedData.push(tmp)
+    })
+    console.log(formattedData)
+    return (
+        <PieChart width={400} height={500}>
+            <Pie data={formattedData} dataKey="time" nameKey='lang' outerRadius={100} label/>
+        </PieChart>
     ) 
 }
 
