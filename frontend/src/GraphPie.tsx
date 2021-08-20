a2b52bfe067b83353d63610b16686434977b32c1
keimoriyama <keischwiiz@gmail.com>
2021-08-20 11:04:18 +0900

    少し調整

--- frontend/src/GraphPie.tsx
+++ frontend/src/GraphPie.tsx
@@ -57,7 +57,7 @@ const GraphPie = (props: Props) => {
     var data = exportData(chartdata)
     const formattedData = formantData(data)
     return (
-        <PieChart width={300} height={300}>
+        <PieChart width={450} height={300}>
             <Legend verticalAlign='top' align='center'/>
             <Pie data={formattedData} dataKey="time" nameKey='lang'outerRadius={100} label={lenderLabel} >
             <Tooltip />
