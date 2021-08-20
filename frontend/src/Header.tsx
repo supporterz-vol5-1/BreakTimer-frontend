508a73801bd06605a4f93798929f77f3360bf566
keimoriyama <keischwiiz@gmail.com>
2021-08-20 10:12:34 +0900

    ヘッダーをアプリ名に変更

--- frontend/src/Header.tsx
+++ frontend/src/Header.tsx
@@ -5,7 +5,7 @@ class Header extends Component{
     render(){
         return (
             <div className='header'>
-                    <div className='headerelement' id='appname'>header</div>
+                    <div className='headerelement' id='appname'>Break Timer</div>
                     <input className='headerelement' type='text' />
             </div>
         )
